1b7a10e9eb2c994996765d889bd4f917b70ae78a
zidhaanfa <zidhaanb@gmail.com>
2022-09-16 01:00:12 +0700

    Tugas Promise

--- promise.js
+++ promise.js
@@ -1,8 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async () => {
+  const xxi = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+  const outout = new Promise((resolve) => {
+    const hasil = xxi.concat(vgc);
+    marah = hasil.filter((item) => item.hasil === "marah").length;
+    tidakMarah = hasil.filter((item) => item.hasil === "tidak marah").length;
+    resolve('marah: ' + marah + ', tidak marah: ' + tidakMarah);
+  });
+  return outout;
+}
+// Masih bingung caranya, hasil sudah keluar
+// tapi benar tidaknya belum tahu
 module.exports = {
   promiseOutput,
 };
