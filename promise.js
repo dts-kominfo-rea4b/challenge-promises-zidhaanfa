58e13180dd10f81d9d1d5073360bf157d0c67e86
zidhaanfa <zidhaanb@gmail.com>
2022-09-16 18:45:52 +0700

    update:)

--- promise.js
+++ promise.js
@@ -1,18 +1,39 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async () => {
+// const promiseOutput = async () => {
+//   const xxi = await promiseTheaterIXX();
+//   const vgc = await promiseTheaterVGC();
+//   const outout = new Promise((resolve) => {
+//     const hasil = xxi.concat(vgc);
+//     marah = hasil.filter((item) => item.hasil === "marah").length;
+//     tidakMarah = hasil.filter((item) => item.hasil === "tidak marah").length;
+//     console.log(tidakMarah);
+//     console.log(marah);
+//   });
+//   return outout;
+// }
+const promiseOutput = async (emosi) => {
   const xxi = await promiseTheaterIXX();
   const vgc = await promiseTheaterVGC();
-  const outout = new Promise((resolve) => {
-    const hasil = xxi.concat(vgc);
-    marah = hasil.filter((item) => item.hasil === "marah").length;
-    tidakMarah = hasil.filter((item) => item.hasil === "tidak marah").length;
-    console.log(tidakMarah);
-    console.log(marah);
-  });
-  return outout;
+  if(emosi === "marah"){
+    const marah = new Promise((resolve) => {
+      const hasil = xxi.concat(vgc);
+      const marah = hasil.filter((item) => item.hasil === "marah").length;
+      resolve(marah);
+    })
+    return marah;
+  }else if(emosi === "tidak marah"){
+    const tidakMarah = new Promise((resolve) => {
+      const hasil = xxi.concat(vgc);
+      const tidakMarah = hasil.filter((item) => item.hasil === "tidak marah").length;
+      resolve(tidakMarah);
+    })
+    return tidakMarah;
+  }
 }
+promiseOutput("tidak marah").then((result) => console.log(result));
+promiseOutput("marah").then((result) => console.log(result));
 // Masih bingung caranya, hasil sudah keluar
 // tapi benar tidaknya belum tahu
 module.exports = {
